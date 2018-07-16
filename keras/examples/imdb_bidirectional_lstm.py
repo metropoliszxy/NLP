8cf22a2ec0f8bb35211c85b940e82189ff3a405d
metropoliszxy <zhaoxinye@nudt.edu.cn>
2018-07-17 07:23:07 +0800

    2018-07-17

--- keras/examples/imdb_bidirectional_lstm.py
+++ keras/examples/imdb_bidirectional_lstm.py
@@ -2,7 +2,13 @@
 
 Output after 4 epochs on CPU: ~0.8146
 Time per epoch on CPU (Core i7): ~150s.
+
+#coding:utf-8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 '''
+
 
 from __future__ import print_function
 import numpy as np
@@ -37,6 +43,7 @@ model.add(Embedding(max_features, 128, input_length=maxlen))
 model.add(Bidirectional(LSTM(64)))
 model.add(Dropout(0.5))
 model.add(Dense(1, activation='sigmoid'))
+
 
 # try using different optimizers and different optimizer configs
 model.compile('adam', 'binary_crossentropy', metrics=['accuracy'])
