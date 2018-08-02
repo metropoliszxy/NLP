7af47672014dad4f07dbd09a577097ee1465f7d7
metropoliszxy <zhaoxinye@nudt.edu.cn>
2018-08-02 20:32:09 +0800

    20180802

--- keras/examples/imdb_cnn.py
+++ keras/examples/imdb_cnn.py
@@ -1,9 +1,13 @@
+#coding=utf-8
+#代码中包含中文，就需要在头部指定编码。
+
 '''This example demonstrates the use of Convolution1D for text classification.
 
 Gets to 0.89 test accuracy after 2 epochs.
 90s/epoch on Intel i5 2.4Ghz CPU.
 10s/epoch on Tesla K40 GPU.
 '''
+
 from __future__ import print_function
 
 from keras.preprocessing import sequence
@@ -12,6 +16,9 @@ from keras.layers import Dense, Dropout, Activation
 from keras.layers import Embedding
 from keras.layers import Conv1D, GlobalMaxPooling1D
 from keras.datasets import imdb
+
+import numpy as np
+
 
 # set parameters:
 max_features = 5000
@@ -23,10 +30,37 @@ kernel_size = 3
 hidden_dims = 250
 epochs = 2
 
-print('Loading data...')
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
 print(len(x_train), 'train sequences')
 print(len(x_test), 'test sequences')
+
+print(x_train)
+
+'''
+#以NumPy专用的二进制类型保存数据，这两个函数会自动处理元素类型和shape等信息，
+#使用它们读写数组就方便多了，但是numpy.save输出的文件很难和其它语言编写的程序读入
+print('Loading data...')
+a=np.arange(5)
+np.save('test.npy',a)
+
+a=np.load('test.npy')
+print(a)
+
+
+
+
+
+
+
+
+
+
+path = 'imdb.npz'
+with np.load(path) as f:
+    x_train, labels_train = f['x_train'], f['y_train']
+    x_test, labels_test = f['x_test'], f['y_test']
+
+
 
 print('Pad sequences (samples x time)')
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
@@ -34,8 +68,13 @@ x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
 print('x_train shape:', x_train.shape)
 print('x_test shape:', x_test.shape)
 
+
+
+#引入sequential，这个就是一个空的网络结构，并且这个结构是一个顺序的序列，所以叫Sequential，Keras里面还有一些其他的网络结构。
 print('Build model...')
 model = Sequential()
+
+
 
 # we start off with an efficient embedding layer which maps
 # our vocab indices into embedding_dims dimensions
@@ -70,3 +109,5 @@ model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=epochs,
           validation_data=(x_test, y_test))
+
+'''
